741dcc3e1e1036500cb7ed0deb4554d546265734
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-09-22 11:01:28 +0530

    two sum

---
+++ TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2,5,5,11};
+        int target = 10;
+
+        // for(int i = 0; i<arr.length-1; i++) {
+        //     for(int j = 1; j<arr.length; j++) {
+        //         if(arr[i] + arr[j] == target) {
+        //             System.out.println(new int[]{i,j};
+        //         }
+        //     }
+        // }
+
+        // Map<Integer, Integer> map= new HashMap<>();
+        // for(int i = 0; i<arr.length-1; i++) {
+        //     int query = target - arr[i];
+        //     if(map.get(query) !=null) {
+        //         return new int[]{i, map.get(query)};
+        //     }
+        //     map.put(arr[i], i);
+        // }
+    }
+}
