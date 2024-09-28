058781332860b7a925816dcf616dc31b3107dcb0
Mohammed Shirhaan <mshirhaan@gmail.com>
2024-09-28 10:11:34 +0530

    .

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -18,3 +18,22 @@ class Solution {
     }
 }
 
+
+//2 pointers
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        while(i<nums.length && nums[i]!=0) {
+            i++;
+        }
+        
+        for(int j = i+1; j<nums.length; j++) {
+            if(nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
