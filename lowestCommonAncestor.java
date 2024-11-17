e70c570683c41e4fbeb8e3267ec7e504b032938b
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2024-11-17 10:14:58 +0530

    Update lowestCommonAncestor.java

--- lowestCommonAncestor.java
+++ lowestCommonAncestor.java
@@ -2,9 +2,6 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         TreeNode current = root;
         while(true) {
-            if(current.val == p.val || current.val == q.val) {
-                return current;
-            }
             if(p.val < current.val && q.val < current.val) {
                 current = current.left;
             } else if(p.val > current.val && q.val > current.val) {
