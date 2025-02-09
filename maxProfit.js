a17a6efe1e8aa9777991d9ef11968384b3ce4aaa
Mohammed Shirhaan <53033299+mshirhaan@users.noreply.github.com>
2025-02-09 11:37:33 +0530

    Update maxProfit.js

--- maxProfit.js
+++ maxProfit.js
@@ -19,3 +19,16 @@ var maxProfit = function(prices) {
         }
     }
 };
+
+//tabulation
+var maxProfit = function(prices) {
+    let profit = 0;
+    let min = prices[0];
+
+    for(let i = 1; i<prices.length; i++) {
+        profit = Math.max(profit, prices[i]-min);
+    
+        if(prices[i]<min) min = prices[i];
+    }
+    return profit;
+};
